49b838cfc8b7880812356f8c14f4a61c80439cf6
vidyendra <iit2022057@iiitl.ac.in>
2023-04-20 19:04:13 +0530

    Bottom Nav Bar

---
+++ app/src/main/java/com/example/bottomnavbar/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.bottomnavbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
